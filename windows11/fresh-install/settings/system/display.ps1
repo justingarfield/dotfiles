2b54d21a69eed3cf5c98784b6682799ae3e83826
Justin Garfield <github@jgarfield.com>
2023-09-24 18:17:01 -0400

    not even close to done2

--- windows11/fresh-install/settings/system/display.ps1
+++ windows11/fresh-install/settings/system/display.ps1
@@ -19,3 +19,34 @@ if (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore\Stor
 if (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\DefaultAccount\Current\default$windows.data.bluelightreduction.bluelightreductionstate\windows.data.bluelightreduction.bluelightreductionstate")) {New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\DefaultAccount\Current\default$windows.data.bluelightreduction.bluelightreductionstate\windows.data.bluelightreduction.bluelightreductionstate" -Type Folder | Out-Null}
 $blueLightReductionState = Scalar-To-Hex "43,42,01,00,0a,02,01,00,2a,06,b0,ad,c0,a8,06,2a,2b,0e,10,43,42,01,00,c6,14,bf,ff,ea,98,e4,da,fb,ec,01,00,00,00,00"
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\DefaultAccount\Current\default$windows.data.bluelightreduction.bluelightreductionstate\windows.data.bluelightreduction.bluelightreductionstate -Name Data -Value ([byte[]]($blueLightReductionState))
+
+### Research
+
+# Strength updates                    HKCU\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\DefaultAccount\Current\default$windows.data.bluelightreduction.settings\windows.data.bluelightreduction.settings\Data
+# Schedule night light On/Off updates HKCU\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\DefaultAccount\Current\default$windows.data.bluelightreduction.settings\windows.data.bluelightreduction.settings\Data
+# Set hours Turn On updates           HKCU\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\DefaultAccount\Current\default$windows.data.bluelightreduction.settings\windows.data.bluelightreduction.settings\Data
+# Set hours Turn Off updates          HKCU\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\DefaultAccount\Current\default$windows.data.bluelightreduction.settings\windows.data.bluelightreduction.settings\Data
+# Turn on now updates                 HKCU\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\DefaultAccount\Current\default$windows.data.bluelightreduction.bluelightreductionstate\windows.data.bluelightreduction.bluelightreductionstate\Data
+# Turn off now updates                HKCU\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\DefaultAccount\Current\default$windows.data.bluelightreduction.bluelightreductionstate\windows.data.bluelightreduction.bluelightreductionstate\Data
+
+$data = (0x43, 0x42, 0x01, 0x00, 0x0A, 0x02, 0x01, 0x00, 0x2A, 0x06)
+$epochTime = [System.DateTimeOffset]::new((date)).ToUnixTimeSeconds()
+$data += $epochTime -band 0x7F -bor 0x80
+$data += ($epochTime -shr 7) -band 0x7F -bor 0x80
+$data += ($epochTime -shr 14) -band 0x7F -bor 0x80
+$data += ($epochTime -shr 21) -band 0x7F -bor 0x80
+$data += $epochTime -shr 28
+$data
+$data += (0x2A, 0x2B, 0x0E, 0x1D, 0x43, 0x42, 0x01, 0x00)
+If ($Enabled) {$data += (0x02, 0x01)}
+$data += (0xCA, 0x14, 0x0E)
+
+#                            constants             epochtime bits constants
+# Off / 0 / Off   - 43,42,01,00,0a,02,01,00,2a,06,|ca,d8,c2,a8,06|,2a,2b,0e|,1c|,43,42,01,00|      ,c2,0a,00,ca,14,0e,01,00,ca,1e,0e,02,00,cf,28,|c8,65|,ca,32,00,ca,3c,00,00,00,00,00
+# Off / 50 / Off  - 43,42,01,00,0a,02,01,00,2a,06,|fd,d9,c2,a8,06|,2a,2b,0e|,1c|,43,42,01,00|      ,c2,0a,00,ca,14,0e,01,00,ca,1e,0e,02,00,cf,28,|94,3c|,ca,32,00,ca,3c,00,00,00,00,00
+# Off / 100 / Off - 43,42,01,00,0a,02,01,00,2a,06,|b0,d9,c2,a8,06|,2a,2b,0e|,1c|,43,42,01,00|      ,c2,0a,00,ca,14,0e,01,00,ca,1e,0e,02,00,cf,28,|e0,12|,ca,32,00,ca,3c,00,00,00,00,00
+#                   43,42,01,00,0a,02,01,00,2a,06,|d1,e4,c2,a8,06|,2a,2b,0e|,1c|,43,42,01,00|      ,c2,0a,00,ca,14,0e,01,00,ca,1e,0e,02,00,cf,28,94,3c,ca,32,00,ca,3c,00,00,00,00,00
+#                   43,42,01,00,0a,02,01,00,2a,06,|e9,e5,c2,a8,06|,2a,2b,0e|,1c|,43,42,01,00|      ,c2,0a,00,ca,14,0e,01,00,ca,1e,0e,02,00,cf,28,e4,42,ca,32,00,ca,3c,00,00,00,00,00
+#                   43,42,01,00,0a,02,01,00,2a,06,|da,e7,c2,a8,06|,2a,2b,0e|,1e|,43,42,01,00|,02,01,c2,0a,00,ca,14,0e,01,00,ca,1e,0e,02,00,cf,28,e4,42,ca,32,00,ca,3c,00,00,00,00,00
+#                   43,42,01,00,0a,02,01,00,2a,06,|f8,e8,c2,a8,06|,2a,2b,0e|,1e|,43,42,01,00|,02,01,c2,0a,00,ca,14,0e,04,00,ca,1e,0e,10,00,cf,28,e4,42,ca,32,00,ca,3c,00,00,00,00,00
+#                   43,42,01,00,0a,02,01,00,2a,06,|b6,e9,c2,a8,06|,2a,2b,0e|,23|,43,42,01,00|,02,01,         ca,14,0e,04,00,ca,1e,0e,10,00,cf,28,e4,42,ca,32,0e,12,2e,2a,00,ca,3c,0e,06,2e,24,00,00,00,00,00
